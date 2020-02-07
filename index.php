21e06513feb0928764557cce2a5a24637ad2c53a
bensuerdgn <50342489+bensuerdgn@users.noreply.github.com>
2020-02-07 23:39:41 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="main.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="header">
+        <div class="header-top">
+            <div class="header-left">
+                <div class="language">
+                    <form action="">
+                        <select name="" id="">
+                            <option value="French">French</option>
+                            <option value="English">English</option>
+                        </select>
+                    </form>
+                </div>
+                <div class="currency">
+                    <form action="">
+                        <select name="" id="">
+                            <option value="EGP">EGP</option>
+                            <option value="EUR">EUR</option>
+                            <option value="USD">USD</option>
+                        </select>
+                    </form>
+                </div>
+                <div>Default welcome msg!</div>
+
+            </div>
+            <div class="header-right">
+                <a href="">My Account</a>
+                <a href="">Checkout</a>
+                <a href="">Blog</a>
+                <a href="">Company</a>
+                <a href="">Log In</a>
+            </div>
+        </div>
+        <div class="header-bottom">
+            <div class="search-box">
+                <form action="">
+                    <input class="search" type="text" placeholder="Search">
+                    <input type="submit" value="">
+                </form>
+            </div>
+            <div class="creta"><h1>CRETA</h1></div>
+            <div class="card"></div>
+        </div>
+    </div>
+    <nav>
+        <div class="menu">
+        <ul>
+            <li>HOME</li>
+            <li>BIRTHDAY</li>
+            <li>SAME DAY</li>
+            <li>GET WELL</li>
+            <li>INTERNATIONAL</li>
+            <li>ANNIVERSARYY</li>
+            <li>GIFTS</li>
+            <li>CUSTOM</li>
+        </ul>
+    </div>
+    
+    </nav>
+</body>
+
+</html>
